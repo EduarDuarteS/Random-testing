c67685d403cb28b7c6a3738ce903247ed796fdf4
Eduard <eduard.duarte@hotmail.com>
2020-09-14 23:20:58 -0500

    Monkey Testing

---
+++ cypress/integration/monkey_testing_ripper.spec.js
@@ -0,0 +1,28 @@
+describe('Wikipedia under monkeys', function() {
+    it('visits wikipedia and survives monkeys', function() {
+        cy.visit('https://es.wikipedia.org/wiki/Wikipedia:Portada');
+        cy.wait(1000);
+        randomClick(10);
+    })
+})
+function randomClick(monkeysLeft) {
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    var monkeysLeft = monkeysLeft;
+    if(monkeysLeft > 0) {
+        cy.get('a').then($links => {
+            var randomLink = $links.get(getRandomInt(0, $links.length));
+            if(!Cypress.dom.isHidden(randomLink)) {
+                cy.wrap(randomLink).click({force: true});
+                monkeysLeft = monkeysLeft - 1;
+            }
+            cy.wait(500);
+            randomClick(monkeysLeft);
+        });
+    }   
+}
